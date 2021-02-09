77875f7178461038906e33f8af780822d2729cfc
Ankit Kumar Gupta <ankitkumargupta00@gmail.com>
2021-02-09 19:01:02 +0530

    user controller

--- backend/controllers/blog.js
+++ backend/controllers/blog.js
@@ -24,4 +24,25 @@ router.post("/api/register", async (req, res) => {
   }
 });
 
+//user login
+
+router.post("/api/login", async (req, res) => {
+  const { email, password, userType } = req.body;
+  try {
+    const user = await User.findOne({ email: email }).exec();
+    if (!user) {
+      res.status(500).json({ success: false, message: "User not exist" });
+    } else if (!Bcrypt.compareSync(password, user.password)) {
+      res
+        .status(500)
+        .json({ success: false, message: "Password does not match" });
+    } else if (userType !== user.userType) {
+      res.status(500).json({ success: false, message: "Invalid user" });
+    } else {
+      res.status(200).json({ success: true, message: user });
+    }
+  } catch (error) {
+    res.status(500).json({ success: false, message: error });
+  }
+});
 export default router;
