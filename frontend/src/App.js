03d522e62101acdd2cee5d1d510539bbad2e7c3b
Ankit Kumar Gupta <ankitkumargupta00@gmail.com>
2021-02-26 09:27:21 +0530

    routes of login and register

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import "./App.css";
 import AddBlog from "./components/AddBlog";
 import Blogs from "./components/Blogs";
 import Header from "./components/Header";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
         <Switch>
           <Route path="/" component={Blogs} exact />
           <Route path="/add" component={AddBlog} />
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
         </Switch>
       </div>
     </BrowserRouter>
