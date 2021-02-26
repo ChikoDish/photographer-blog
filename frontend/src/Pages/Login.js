701690e2fa04d2c17cf1274be41c45c765c787a9
Ankit Kumar Gupta <ankitkumargupta00@gmail.com>
2021-02-26 09:20:29 +0530

    login page

---
+++ frontend/src/Pages/Login.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+import React, { useState } from "react";
+import { loginUri } from "../utils/apis";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const login = (e) => {
+    axios.post(loginUri, { email, password }).then((response) => {
+      console.log(response);
+    });
+  };
+  return (
+    <div className="login">
+      <h1>Login</h1>
+      <form onSubmit={login}>
+        <label>Email</label>
+        <input type="email" onChange={(e) => setEmail(e.target.value)} />
+        <label>Password</label>
+        <input type="password" onChange={(e) => setPassword(e.target.value)} />
+        <button className="button">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
--- frontend/src/utils/apis.js
+++ frontend/src/utils/apis.js
@@ -3,3 +3,5 @@ import { backpath } from "./backpath";
 export const add = `${backpath}blog/api/addblog`;
 
 export const getBlogs = `${backpath}blog/api/getblogs`;
+
+export const loginUri = `${backpath}blog/api/login`;
